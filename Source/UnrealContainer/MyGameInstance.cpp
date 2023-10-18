265ca47370ca0369756f293b435275c528ace10a
psychehose <psychehose@gmail.com>
2023-10-18 17:00:02 +0900

    Array and Set

---
+++ Source/UnrealContainer/MyGameInstance.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+#include "Algo/Accumulate.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	const int32 ArrayNum = 10;
+
+	TArray<int32> Int32Array;
+
+	for (int i = 1; i <= ArrayNum; ++i)
+	{
+		Int32Array.Add(i);
+
+		// or
+		// Int32Array.Emplace(i);
+
+	}
+
+	// remove even number
+
+	Int32Array.RemoveAll(
+		// using lamda
+
+		[](int32 Val)
+		{
+			return Val % 2 == 0;
+		}
+	);
+
+	Int32Array += { 2, 4, 6, 8, 10 };
+	// Original C++ style (using memcpy)
+
+	TArray<int32> Int32ArrayCompare;
+
+	int32 CArray[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+	Int32ArrayCompare.AddUninitialized(10);
+
+	FMemory::Memcpy(Int32ArrayCompare.GetData(), CArray, sizeof(int32)*ArrayNum);
+
+	ensure(Int32Array == Int32ArrayCompare);
+
+	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
+	ensure(55 == SumByAlgo);
+
+	TSet<int32> Int32Set;
+	for (int32 i = 1; i <= ArrayNum; ++i) {
+		Int32Set.Add(i);
+	}
+
+	Int32Set.Remove(2);
+	Int32Set.Remove(4);
+	Int32Set.Remove(6);
+	Int32Set.Remove(8);
+	Int32Set.Remove(10);
+
+	Int32Set.Add(2);
+	Int32Set.Add(4);
+	Int32Set.Add(6);
+	Int32Set.Add(8);
+	Int32Set.Add(10);
+
+
+
+}
---
+++ Source/UnrealContainer/UnrealContainerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealContainerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCONTAINER_API AUnrealContainerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
